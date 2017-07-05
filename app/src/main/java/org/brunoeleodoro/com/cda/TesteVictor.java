77e59be5adbec32958cc55845f9b33f68f63f910
bruno <brunoeleodoro96@gmail.com>
2017-07-05 15:21:10 -0300

    kdkmdsklmnaflsd

--- app/src/main/java/org/brunoeleodoro/com/cda/TesteVictor.java
+++ app/src/main/java/org/brunoeleodoro/com/cda/TesteVictor.java
@@ -1,8 +1,14 @@
 package org.brunoeleodoro.com.cda;
 
+import android.widget.Toast;
+
 /**
  * Created by bruno on 05/07/17.
  */
 
 public class TesteVictor {
+    public void show()
+    {
+        //Toast.makeText///, "", Toast.LENGTH_SHORT).show();
+    }
 }
